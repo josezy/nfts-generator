36bac2814905fd593e313f874212d68f7c685213
Jose B <jose.zdy@gmail.com>
2021-10-30 16:07:18 -0500

    rooney traits

--- athletes/rooney/__init__.py
+++ athletes/rooney/__init__.py
@@ -1,11 +1,11 @@
 
 TRAITS = {
-    "BASE": {
+    "base": {
         "Standard": 80,
         "Old": 15,
         "Young": 5,
     },
-    "HEAD": {
+    "head": {
         None: 54,
         "BA Snapback": 14,
         "Wooly Hat": 13,
@@ -13,26 +13,26 @@ TRAITS = {
         "Title Crown": 6,
         "Crown": 1,
     },
-    "FACE": {
+    "face": {
         None: 68,
         "Sweat": 20,
         "England Paint": 10,
         "Ad Beard": 2,
     },
-    "MOUTH": {
+    "mouth": {
         None: 72,
         "Gold Mouthguard": 12,
         "Medal": 11,
         "Diamond Grill": 4.75,
         "Dosbrak Bandana": 0.25,
     },
-    "EYE": {
+    "eye": {
         None: 62,
         "Wayfarers": 18,
         "Bloody Eye": 13,
         "Laser Eyes": 7,
     },
-    "ACCESSORIES": {
+    "accessories": {
         None: 15,
         "Captain Armband": 15,
         "Yellow Ball": 14,
@@ -45,7 +45,7 @@ TRAITS = {
         "Boot Award": 2.5,
         "Diamond Ball": 1,
     },
-    "CLOTHES": {
+    "clothes": {
         "White Jersey": 20,
         "Red Jersey": 19,
         "Blue Jersey": 14,
@@ -53,11 +53,11 @@ TRAITS = {
         "Training Hoodie": 11,
         "Black Jersey": 10,
         "Managers Jacket": 6,
-        "Flag": 5,
+        "Patriot Flag": 5,
         "Red Jersey Torn": 2,
         "Astronaut": 1,
     },
-    "BACKGROUNDS": {
+    "Backgrounds": {
         "Green": 20,
         "Black": 20,
         "Red": 20,
@@ -67,14 +67,29 @@ TRAITS = {
         "Team": 4.2,
         "Stadium": 0.8,
     },
+    "Signature": {
+        "Signature": 100,
+    },
 }
 
 def conditions(nft_traits):
 
-    if nft_traits['BASE'] == 'Young' and nft_traits['CLOTHING'] == 'Astronaut':
-        nft_traits['CLOTHING'] = 'Red Jersey Torn'
+    if nft_traits['base'] == 'Young' and nft_traits['clothes'] == 'Astronaut':
+        nft_traits['clothes'] = 'Red Jersey Torn'
 
-    if nft_traits['BASE'] == 'Old' and nft_traits['FACE'] == 'England Paint':
-        nft_traits['FACE'] = None
+    if nft_traits['base'] == 'Old' and nft_traits['face'] == 'England Paint':
+        nft_traits['face'] = None
+
+    if nft_traits['face'] == 'England Paint':
+        nft_traits['accessories'] = None
+
+    if nft_traits['clothes'] == 'Astronaut' and nft_traits['accessories'] == 'Captain Armband':
+        nft_traits['accessories'] = None
+
+    if nft_traits['accessories'] == 'League Trophy':
+        nft_traits['clothes'] = 'Red Jersey'
+
+    if nft_traits['face'] == 'England Paint':
+        nft_traits['clothes'] = None
 
     return nft_traits
