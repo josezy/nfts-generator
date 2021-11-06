83f344a50680fcd4a39029a63aaf6821ce47f0b3
Jose B <jose.zdy@gmail.com>
2021-11-06 17:04:45 -0500

    update fixing-script

--- shuffle-fix/return-tokens.ts
+++ shuffle-fix/return-tokens.ts
@@ -1,26 +1,43 @@
-import { execSync } from "child_process";
 import fs from 'fs';
+import { execSync } from "child_process";
 
 import holders from './holders.json'
 
 const TOKEN = 'Aj2HHSuPg1WmyAa6eAyS2uamESPomsoGsi56LXjNyXrb'
 const AMOUNT = 1 // KEEP ONE TOKEN PER ACCOUNT
-const FEE_PAYER = '~/.config/solana/legends.json'
+const KEYPAIR = '~/.config/solana/legends.json'
 
+const RUST_PATH = '~/Desktop/blockasset/metaplex/rust/target/debug'
+const RPC_ENDPOINT = 'https://cmnftdrop7299eedc071c.genesysgo.net/'
+
+const new_md_name = 'Exiled Lomus'
 
 for (const holder of holders) {
-  console.log(`Returning token to ${holder.owner_wallet}`)
-  const cmd = `spl-token transfer --fee-payer ${FEE_PAYER} ${TOKEN} ${AMOUNT} ${holder.owner_wallet}`
-  try {
-    const result = execSync(cmd)
-    console.log(result)
-    holder['success'] = true
-  } catch(e) {
-    holder['success'] = false
+
+  if (!holder['metadataUpdated']) {
+    console.log(`Updating metadata for mint ${holder.mint_account}`)
+    const metadataCmd = `${RUST_PATH}/metaplex-token-metadata-test-client update_metadata_accounts --url ${RPC_ENDPOINT} --mint ${holder.mint_account} --keypair ${KEYPAIR} --name ${new_md_name}`
+    try {
+      const resultMd = execSync(metadataCmd)
+      console.log(resultMd)
+      holder['metadataUpdated'] = true
+    } catch(e) {
+      holder['metadataUpdated'] = false
+    }
   }
 
-  fs.writeFileSync('./output.json', JSON.stringify(holders));
+  // if (!holder['tokenReturned']) {
+  //   console.log(`Returning token to ${holder.owner_wallet}`)
+  //   try {
+  //     const tokenCmd = `spl-token transfer --fund-recipient --fee-payer ${KEYPAIR} ${TOKEN} ${AMOUNT} ${holder.owner_wallet}`
+  //     const resultToken = execSync(tokenCmd)
+  //     console.log(resultToken)
+  //     holder['tokenReturned'] = true
+  //   } catch(e) {
+  //     holder['tokenReturned'] = false
+  //   }
+  // }
+
+  fs.writeFileSync('./processed_holders.json', JSON.stringify(holders));
 }
-
-
 
